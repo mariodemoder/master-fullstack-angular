74f230e9fd0483b5f94f55e5faecc53335876c9d
mariodemoder <mariodemoder@gmail.com>
2023-02-06 13:20:06 -0300

    iniioc angular

---
+++ src/app/models/users.ts
@@ -0,0 +1,13 @@
+
+export class User{
+    constructor(
+            public  idi: number,
+            public  name: string,
+            public  surname: string,
+            public  role: string,
+            public  email: string,
+            public  password: string,
+            public  descripcion: string,
+            public  image: string
+    ){}
+}
